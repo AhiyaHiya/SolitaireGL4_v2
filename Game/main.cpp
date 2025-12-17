57a84d842067c4cca0c3e958faffa54ed92eb024
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-12-17 16:45:30 -0700

    Add basic glfw init terminate calls

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,10 +1,24 @@
+// clang-format off
+#include <glad/gl.h>
+#include <GLFW/glfw3.h> // Ordering is important and this file must be included after glad
+// clang-format on
+
+#include <iostream>
+
+constexpr auto generic_error = 1;
+constexpr auto no_error      = 0;
 
 // ----------------------------------------------------------------
 /// @brief No description needed
 /// @return 0 for no error, everything else is error
 int main()
 {
-    // Game loop would go here
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW\n";
+        return generic_error;
+    }
 
-    return 0;
+    glfwTerminate();
+    return no_error;
 }
