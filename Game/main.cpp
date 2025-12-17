09d42f10e83fd342d45a1a7aa00a0e09e40ba969
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-12-17 16:42:54 -0700

    Add empty main file

---
+++ Game/main.cpp
@@ -0,0 +1,10 @@
+
+// ----------------------------------------------------------------
+/// @brief No description needed
+/// @return 0 for no error, everything else is error
+int main()
+{
+    // Game loop would go here
+
+    return 0;
+}
