b7deb43f4f43904a56e550619a1f9ce90607ada6
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2025-12-18 17:11:29 -0700

    Add empty CardRenderer files

---
+++ Game/CardRenderer.cpp
@@ -0,0 +1,7 @@
+#include "CardRenderer.hpp"
+
+auto create_card_renderer() -> std::expected<CardRenderer, std::string>
+{
+    // Not implemented yet
+    return std::unexpected("Not implemented yet");
+}
---
+++ Game/CardRenderer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Types.hpp"
+
+// clang-format off
+#include <glad/gl.h>
+#include <glm/glm.hpp>
+// clang-format on
+
+#include <expected>
+#include <vector>
+
+struct CardRenderer
+{
+    GLuint shader_program = 0;
+    GLuint texture_array  = 0; // Still fine to call it this, or rename to texture_atlas
+    GLuint vao            = 0;
+    GLuint vbo            = 0;
+
+    GLint uProjection = -1;
+    GLint uPosition   = -1;
+    GLint uSize       = -1;
+    GLint uTexRect    = -1; // This one is now used!
+
+    std::vector<glm::vec4> uv_rects; // One per card (including back)
+};
+
+auto create_card_renderer() -> std::expected<CardRenderer, std::string>;
