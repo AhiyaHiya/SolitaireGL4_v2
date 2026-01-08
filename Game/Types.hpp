72e1b111df8cf5a4de36c14c24098302f2c1b5a4
Jaime O. Rios <3794310+AhiyaHiya@users.noreply.github.com>
2026-01-07 17:38:38 -0700

    Update Types

--- Game/Types.hpp
+++ Game/Types.hpp
@@ -3,9 +3,12 @@
 #include <cstdint>
 #include <string>
 #include <unordered_map>
+#include <utility>
 
 using error_code_t    = std::int32_t;
 using error_message_t = std::string;
+
+using atlas_pos_t = std::int32_t;
 
 struct Frame
 {
@@ -14,4 +17,7 @@ struct Frame
     std::int32_t w;
     std::int32_t h;
 };
-using Frames = std::unordered_map<std::string, Frame>;
+using Frames = std::unordered_map<std::string, std::pair<Frame, atlas_pos_t>>;
+
+// <card name, position index>
+using FramesIndex = std::unordered_map<std::string, std::int32_t>;
